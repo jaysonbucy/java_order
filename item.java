1c6a73038278244de68032c1130d559654019751
jayson.h.b@gmail.com <jayson.h.b@gmail.com>
2018-03-25 12:40:33 -0500

    wip

--- item.java
+++ item.java
@@ -1,40 +1,33 @@
-package com.exam;
 /**
 * Represents a part or service that can be sold.
-*
 * care should be taken to ensure that this class is immutable since it
 * is sent to other systems for processing that should not be able to
 * change it in any way.
-*
-* &lt;p&gt;Copyright: Copyright (c) 2004&lt;/p&gt;
-* &lt;p&gt;Company: Exams are us&lt;/p&gt;
-* @author Joe Blow
-* @version 1.0
 */
-public class Item
-{
-private Integer key;
-private String name;
-private float price;
+package com.exam;
 
+public class Item {
 
-public Item(Integer key, String name, float price)
-{
-this.key = key;
-this.name = name;
-this.price = price;
-}
+  private Integer key;
+  private String name;
+  private float price;
 
-public Integer getKey()
-{
-return key;
-}
-public String getName()
-{
-return name;
-}
-public float getPrice()
-{
-return price;
-}
+  public Item(Integer key, String name, float price)  {
+    this.key = key;
+    this.name = name;
+    this.price = price;
+  }
+
+  public Integer getKey()  {
+    return key;
+  }
+  
+  public String getName()  {
+    return name;
+  }
+
+  public float getPrice()  {
+    return price;
+  }
+
 }
--- order.java
+++ order.java
@@ -1,38 +1,28 @@
-package com.exam;
-import java.util.*;
 /**
 * Represents and Order that contains a collection of items.
-*
 * care should be taken to ensure that this class is immutable since it
 * is sent to other systems for processing that should not be able
 * to change it in any way.
-*
-* &lt;p&gt;Copyright: Copyright (c) 2004&lt;/p&gt;
-* &lt;p&gt;Company: Exams are us&lt;/p&gt;
-* @author Joe Blow
-* @version 1.0
 */
-public class Order
-{
-private OrderItem[] orderItems;
-public Order(OrderItem[]orderItems)
-{
-this. orderItems = orderItems;
-}
-// Returns the total order cost after the tax has been applied
-public float getOrderTotal(float taxRate)
-{
-return 0; // implement this method
-}
-/**
-* Returns a Collection of all items sorted by item name
+package com.exam;
+import java.util.*;
+import java.io.Serializable;
 
-* (case-insensitive).
-*
-* @return Collection
-*/
-public Collection getItems()
-{
-return null; // implement this method
-}
+public class Order implement Serializable {
+
+  private OrderItem[] orderItems;
+  
+  public Order(OrderItem[]orderItems)  {
+    this. orderItems = orderItems;
+  }
+
+  // Returns the total order cost after the tax has been applied
+  public float getOrderTotal(float taxRate)  {
+    return 0; // implement this method
+  }
+
+  //Returns a Collection of all items sorted by item name (case-insensitive).
+  public Collection getItems()  {
+    return null; // implement this method
+  }
 }
